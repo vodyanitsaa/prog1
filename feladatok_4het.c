8cb70c76a66af70377480a06216a395a2b235676
vodyanitsaa <csengebiro28@gmail.com>
2026-03-10 17:12:30 +0100

    Add files via upload

---
+++ feladatok_4het.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <time.h>
+
+//1.feladat
+int valid_triangle(int a, int b, int c){
+    if(a+b<c || a+c<b || b+c<a){
+        return 0;
+    }
+    return 1;
+}
+//2.feladat
+int is_even(int szam){
+    if(szam%2==0){
+        return 1;
+    }
+    return 0;
+}
+int is_odd(int szam){
+    if(is_even(szam)==0){
+    return 1;
+    }
+    return 0;
+}
+//3.feladat
+int tomb_rendezett(int T[],int m){
+    for(int i=0;i<m-1;++i){
+        if(T[i]>T[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+//4.feladat
+int tomb_min(int T[],int m){
+    int min=T[0];
+    for(int i=1;i<m;++i){
+        if(T[i]<min){
+            min=T[i];
+        }
+    }
+    return min;
+}
+int tomb_max(int T[],int m){
+    int max=T[0];
+    for(int i=1;i<m;++i){
+        if(T[i]>max){
+            max=T[i];
+        }
+    }
+    return max;
+}
+//5.feladat
+int contains(int T[],int m,int szam){
+    for(int i=0;i<m;++i){
+        if(T[i]==szam){
+            return 1;
+        }
+    }
+    return 0;
+}
+//6.feladat
+int indexx(int T[], int m, int szam){
+    for(int i=0;i<m;++i){
+        if(T[i]==szam){
+            return i;
+        }
+    }
+    return -1;
+}
+//7.feladat
+int felhokarcolo(int T[],int m){
+    int osszeg=0;
+    for(int i=0;i<m-1;++i){
+        if(T[i]-T[i+1]<0){
+            osszeg+=T[i+1]-T[i];
+        }else{
+            osszeg+=T[i]-T[i+1];
+        }
+    }
+    return osszeg;
+}
+//8.feladat
+int vesszo(int T[],int m){
+    for(int i=0;i<m-1;++i){
+        printf("%d,",T[i]);
+    }
+    printf("%d", T[m-1]);
+}
+//9.feladat
+int abszolut(int T[], int m){
+    for(int i=0;i<m;++i){
+        if(T[i]<0){
+            T[i]=T[i]*-1;
+        }
+        printf("%d ",T[i]);
+    }
+}
+
+
+int main(){
+    printf("1.feladat %d\n", valid_triangle(1,2,10));
+
+    int szam=45;
+    printf("2.feladat even:%d odd:%d\n",is_even(szam), is_odd(szam));
+
+    int T[7]={6,2,3,7,-2,8,45};
+    int m=7;
+    printf("3.feladat %d\n",tomb_rendezett(T,m));
+
+    printf("4.feladat min:%d max:%d\n",tomb_min(T,m),tomb_max(T,m));
+
+    printf("5.feladat %d\n",contains(T,m,szam));
+
+    printf("6.feladat %d\n",indexx(T,m,szam));
+    
+    int T2[]={1, 7, 9, 7, 6, 9, 3, 1, 3, 4, 8, 6, 2, 3, 1, 5, 9, 0, 7, 7, 2, 9, 3, 0, 5, 1, 9, 0, 7, 8, 9, 0, 2, 4, 7, 3, 3, 6, 1, 7, 9, 7, 6, 9, 7, 8, 9, 4, 2, 3, 0, 6, 5, 7, 2, 7, 3, 4, 3, 0, 0, 8, 1, 1, 5, 7, 7, 3, 2, 6, 7, 5, 8, 0, 5, 5, 0, 0, 9, 6, 3, 1, 3, 2, 7, 0, 8, 4, 7, 7, 3, 2, 2, 4, 0, 7, 5, 3, 6, 0, 2, 1, 1, 2, 0, 1, 1, 3, 8, 7, 9, 8, 7, 1, 3, 9, 3, 3, 5, 7, 6, 5, 8, 7, 8, 9, 7, 6, 8, 8, 1, 4, 4, 1, 6, 6, 2, 2, 4, 9, 2, 8, 4, 7, 4, 3, 0, 6, 3, 9, 4, 7, 4, 1, 2, 4, 3, 7, 7, 7, 6, 7, 8, 9, 3, 4, 2, 4, 8, 6, 5, 4, 8, 5, 2, 7, 6, 3, 0, 2, 2, 1, 9, 6, 0, 1, 2, 4, 6, 0, 9, 4, 1, 1, 9, 4, 5, 3, 0, 8, 2, 9, 5, 2, 0, 8, 5, 0, 0, 5, 7, 6, 8, 8, 3, 8, 1, 5, 0, 6, 8, 2, 3, 4, 2, 4, 6, 2, 8, 8, 1, 4, 7, 3, 9, 1, 3, 1, 1, 0, 5, 4, 0, 8, 2, 7, 2, 3, 7, 1, 6, 3, 3, 5, 0, 5, 1, 0, 6, 8, 4, 5, 8, 6, 2, 9, 8, 2, 3, 9, 9, 4, 7, 2, 4, 5, 9, 3, 8, 4, 7, 9, 7, 1, 6, 3, 0, 4, 8, 3, 5, 3, 5, 6, 3, 2, 9, 6, 2, 4, 2, 2, 4, 1, 3, 7, 2, 1, 6};
+    int m2=309;
+    printf("7.feladat %d\n", felhokarcolo(T2,m2));
+
+    printf("8.feladat ");
+    printf("\n", vesszo(T,m));
+
+    printf("9.feladat\n");
+    for(int i=0;i<m;++i){
+        printf("%d ",T[i]);
+    }
+    printf("\n");
+    printf("\n",abszolut(T,m));
+    
+    time_t t = time(NULL);
+    struct tm *inf = localtime(&t);
+    char ev[5];
+    strftime(ev, sizeof(ev), "%Y", inf);
+    printf("%s\n", ev);
+
+
+    return 0;
+}
